3c6d46d5b0d677b17c82c4e234fca5a433dc8f72
Talesign <o.taleghani@talesign.com>
2024-01-02 16:07:14 +0100

    basic keympas & options

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,3 +1,19 @@
 -- Basic remaps
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+-- Navigate windows 
+vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", {} )
+vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", {} )
+vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", {} )
+vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", {} )
+
+-- Navigate tabs
+vim.api.nvim_set_keymap("n", "<F2>", "<ESC>:tabnew<CR>", {})
+vim.api.nvim_set_keymap("n", "<F3>", "gT", {})
+vim.api.nvim_set_keymap("n", "<F4>", "gt", {})
+vim.api.nvim_set_keymap("n", "<F5>", "<cmd>q<cr>", {})
+
+-- Vertical navigation
+--    max 8 lines top // the primagen
+--    center navigation when using {} --> zz
--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,1 +1,17 @@
--- mapleader solitamente messo qui non so il why
+-- General commands
+vim.opt.timeout = false
+vim.opt.relativenumber = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.autoindent = true
+
+-- Tabs
+tab = 2
+vim.opt.tabstop = tab
+vim.opt.softtabstop = tab
+vim.opt.shiftwidth = tab
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+
+-- Text highlight
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
